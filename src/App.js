4414a2336aac65bb9a28fd4f73840e8956403c70
Tara Bell <ttriplett.prsvr@Gmail.com>
2024-07-23 14:27:57 -0400

    "Markdown Previewer"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+
+const defaultMarkdown = `
+# React Markdown Previewer 
+
+## Markdown Examples:
+
+# Header
+
+## Sub-Header
+
+[Link](https://www.freecodecamp.com)
+
+\`<div>Inline Code</div>\`
+
+\`\`\`
+// This is
+// a
+// Code Block
+\`\`\`
+
+> Block Quote
+
+**Bold Text**
+
+_Italic Text_
+
+**_Bold AND Italic Text!_**
+
+- List with
+  - different
+    - Indentation levels
+
+![freeCodeCamp logo in camel-case with a flame in parentheses](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [markdownText, setMarkdownText] = useState(defaultMarkdown);
 
+    return (
+    <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>Markdown Previewer</h1>
+        <div className="boxes-container">
+          <textarea name="editor" id="editor" value={markdownText} onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+          <div id="preview">
+            <ReactMarkdown>{markdownText}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default App;
+
